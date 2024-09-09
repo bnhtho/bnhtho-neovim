169f2185614af9af495d56dbf50540765059b9e6
Bui Nguyen Hoang Tho <thohnb@gmail.com>
2024-09-09 22:40:51 +0700

    Add autopair

--- lua/plugins/utils/init.lua
+++ lua/plugins/utils/init.lua
@@ -15,9 +15,30 @@ return
   },
   {
     "camspiers/snap",
-    dependencies = {
-      "mfussenegger/nvim-fzy"
-    },
-  opt={}
-  }
+  },
+  -- mini.move
+    { 'echasnovski/mini.move', version = false,
+    config = function()
+      require('mini.move').setup({
+        -- Rebind mapping
+        mappings = {
+          -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+          left = '<M-Left>',
+          right = '<M-Right>',
+          down = '<M-Down>',
+          up = '<M-Up>',
+      
+          -- Move current line in Normal mode
+          line_left = '<M-Left>',
+          line_right = '<M-Right>',
+          line_down = '<M-Down>',
+          line_up = '<M-Up>',
+        },
+      }
+      )
+    end
+  },
+  {"altermo/ultimate-autopair.nvim",
+    event = { "InsertEnter", "CmdlineEnter" },
+    opts = {},}
 }
