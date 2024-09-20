903a005dcbd3671a3b0dd4e5260cc015549788ec
Bui Nguyen Hoang Tho <thohnb@gmail.com>
2024-09-20 17:10:16 +0700

    Optimize keybind for switch tab

--- lua/config/keybind.lua
+++ lua/config/keybind.lua
@@ -28,18 +28,17 @@ keymap.set("n", "<leader>w", "<CMD>bdelete<cr>")
 
 keymap.set("n", "D", "<Cmd>copy.<Cr>")
 keymap.set("x", "<C-d>", ":copy.-1<Cr>gv")
--- J
 if vim.o.wrap then
 	keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { expr = true })
 	keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { expr = true })
 end
-
 keymap.set('n', 'q:', function() end)
 keymap.set('n', '<leader>t', "<CMD>Neotree toggle <CR>")
 -- Delete floating windows
 keymap.set('n', '<ESC>', "<cmd>:only<cr>")
-
--- Toggle terminal
-keymap.set('n', '<leader>`', "<CMD>ToggleTerm<CR>")
-
-keymap.set('n', '<Tab>', '<cmd>:bnext<cr>', { noremap = true, silent = true })
+-- Alt+H/L to next buffer/previous buffer
+keymap.set('n', '<M-l>', '<cmd>:bnext<cr>', { noremap = true, silent = true })
+keymap.set('n', '<M-h>', '<cmd>:bprev<cr>', { noremap = true, silent = true })
+vim.opt.wildcharm = vim.fn.char2nr('\26')
+-- Map <Tab> to trigger buffer switching with wildcharm
+keymap.set('n', '<Tab>', ':buffer ', { noremap = true, silent = false })
