1fbedf526a718a5e5fdc5b0a766522176cd83a5a
Bui Nguyen Hoang Tho <thohnb@gmail.com>
2024-09-22 13:31:29 +0700

    75% - Highlight toggle

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -19,13 +19,46 @@ function M.pick_buffer_and_split(split_cmd)
         print("No buffer chosen, keeping empty buffer.")
     end
 end
--- Pick todo-hightlight
--- Utility function to toggle comment
--- Function to toggle comment using Comment.nvim
-local function toggle_comment(line_number)
-    -- Use Comment.nvim's API to toggle comment for the current line
-    require('Comment.api').toggle.linewise.current(line_number)
+
+
+local function str_length(s)
+    return string.len(s)
 end
+
+local function str_length(s)
+    return string.len(s)
+end
+
+local function toggle_comment(highlight)
+    print(highlight)
+
+    -- Get the current cursor position
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local line_number = cursor_pos[1]
+    local col_number = cursor_pos[2]
+
+    -- Get the current line
+    local line = vim.api.nvim_get_current_line()
+
+    -- Check if the highlight exists in the line
+    local highlight_start = line:find(highlight, col_number + 1) -- Find highlight starting from cursor position
+    if highlight_start then
+        -- Highlight exists, remove it
+        local new_line = line:sub(1, highlight_start - 1) .. line:sub(highlight_start + str_length(highlight))
+        vim.api.nvim_set_current_line(new_line)
+
+        -- Move cursor back to the position before the highlight
+        vim.api.nvim_win_set_cursor(0, { line_number, highlight_start - 1 })
+    else
+        -- Highlight does not exist, add it
+        local new_line = line:sub(1, col_number) .. highlight .. ' ' .. line:sub(col_number + 2) .. ''
+        vim.api.nvim_set_current_line(new_line)
+        -- Comment that new_line
+        require('Comment.api').toggle.linewise.current(new_line)
+        -- Move cursor to the position after the inserted highlight :
+        vim.api.nvim_win_set_cursor(0, { line_number, col_number + str_length(highlight) + str_length(highlight) })
+    end
+end 
 
 function M.pick_todo()
     vim.ui.select({'TODO', 'FIXME'}, {
@@ -33,22 +66,8 @@ function M.pick_todo()
     }, 
     function(selected)
         if selected then
-            -- Get the current cursor position
-            local cursor_pos = vim.api.nvim_win_get_cursor(0) 
-            local line_number = cursor_pos[1]
-            local col_number = cursor_pos[2]
-
-            -- Insert the selected text at the cursor position
-            vim.api.nvim_buf_set_text(0, line_number - 1, col_number, line_number - 1, col_number, {selected})
-
-            -- Toggle comment using Comment.nvim
-            toggle_comment(line_number)
-        else
-            print("No selection made")
+            toggle_comment(selected)
         end
     end)
 end
-
-
-
 return M
