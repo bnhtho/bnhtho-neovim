69e3c992f51756767b1bb544151c0e9ba9af0884
Bui Nguyen Hoang Tho <thohnb@gmail.com>
2024-09-22 22:09:38 +0700

    Add telescope theme-ivy

--- lua/plugins/ui/telescope.lua
+++ lua/plugins/ui/telescope.lua
@@ -1,12 +1,16 @@
 return {
+	-- // NOTE: Will do tmr
 	'nvim-telescope/telescope.nvim',
 	dependencies = {
 		'nvim-lua/plenary.nvim',
 		"stevearc/dressing.nvim",
+	},
 		opts = {
-			defaults = {
-				preview = false,
-			}
+			pickers = {
+				live_grep = {
+				  theme = "ivy",
+				  previewer = true,
+				}
+			  },
 		}
 	}
-}
