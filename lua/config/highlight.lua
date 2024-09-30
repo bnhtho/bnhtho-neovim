ffb6d75dbe4299740dacbd66021777b517a8be8c
Bui Nguyen Hoang Tho <thohnb@gmail.com>
2024-09-30 19:50:36 +0700

    Change telescope keybind

--- lua/config/highlight.lua
+++ lua/config/highlight.lua
@@ -2,7 +2,7 @@
 -- ║    Highlights         ║
 -- ╚═══════════════════════╝
 -- Code Completetion
-local highlight = hightlight
+local highlight = vim.api.nvim_set_hl
 
 highlight(0, 'CmpItemAbbrDeprecated', { bg = 'NONE', strikethrough = true, fg = '#808080' })
 highlight(0, 'CmpItemAbbrMatch', { bg = 'NONE', fg = '#569CD6' })
@@ -30,15 +30,15 @@ highlight(0, 'NormalFloat', { bg = 'NONE' })
 -- ╔═══════════════════════╗
 -- ║    Tabline            ║
 -- ╚═══════════════════════╝
-hightlight(0, 'MiniTablineFill', { bg = '#292828' })
-hightlight(0, 'MiniTablineCurrent', { bg = '#292828', fg = '#D2A655' })         -- Active buffer
-hightlight(0, 'MiniTablineVisible', { bg = '#292828', fg = 'NONE' })            -- Visible buffer
-hightlight(0, 'MiniTablineHidden', { bg = '#292828', fg = '#a0a0a0' })          -- Inactive buffer (gray)
-hightlight(0, 'MiniTablineModifiedCurrent', { bg = '#3c3c3c', fg = '#D2A655' }) -- Active modified buffer
-hightlight(0, 'MiniTablineModifiedVisible', { bg = '#292828', fg = '#D2A655' }) -- Visible modified buffer
-hightlight(0, 'MiniTablineFill', { bg = '#292828' })                            -- Fill area
-hightlight(0, 'MiniTablineTabpagesection', { bg = '#292828', fg = '#ffffff' })  -- Tabpage section
-hightlight(0, 'FloatBorder', { bg = 'NONE', fg = '#a0a0a0' })
+vim.api.nvim_set_hl(0, 'MiniTablineFill', { bg = '#292828' })
+vim.api.nvim_set_hl(0, 'MiniTablineCurrent', { bg = '#292828', fg = '#80AA9E' })         -- Active buffer
+vim.api.nvim_set_hl(0, 'MiniTablineVisible', { bg = '#292828', fg = 'NONE' })            -- Visible buffer
+vim.api.nvim_set_hl(0, 'MiniTablineHidden', { bg = '#292828', fg = '#a0a0a0' })          -- Inactive buffer (gray)
+vim.api.nvim_set_hl(0, 'MiniTablineModifiedCurrent', { bg = '#292828', fg = '#D2A655' }) -- Active modified buffer
+vim.api.nvim_set_hl(0, 'MiniTablineModifiedVisible', { bg = '#292828', fg = '#D2A655' }) -- Visible modified buffer
+vim.api.nvim_set_hl(0, 'MiniTablineModifiedHidden', { bg = '#292828', fg = '#a0a0a0' }) -- Unfocus modified buffer
+vim.api.nvim_set_hl(0, 'MiniTablineFill', { bg = '#292828' })                            -- Fill area
+vim.api.nvim_set_hl(0, 'MiniTablineTabpagesection', { bg = '#292828', fg = '#ffffff' })  -- Tabpage section
 -- ╔═══════════════════════╗
 -- ║    Plugins            ║
 -- ╚═══════════════════════╝
--- lua/config/keybind.lua
+++ lua/config/keybind.lua
@@ -2,7 +2,6 @@
 -- ╔═══════════════════════╗
 -- ║    Local Variables    ║
 -- ╚═══════════════════════╝
-
 local keymap = vim.keymap
 local custom_key = require('config.utils')
 local builtin = require('telescope.builtin')
@@ -18,7 +17,7 @@ keymap.set("n", "<leader>l", "<C-w>l", { desc = "Focus Right Window" })
 
 -- Select All Text
 keymap.set("n", "<leader>a", "gg<S-v>G", { desc = "Select All" })
-keymap.set("n", "<C-s>", ":w<cr>",{desc = "Save File"})
+keymap.set("n", "<C-s>", ":w<cr>", { desc = "Save File" })
 -- Split windows
 keymap.set('n', 'sj', ':split<cr><C-w>w', { desc = "Split Down" })
 keymap.set('n', 'sl', ':vsplit<cr><C-w>w', { desc = "Split Right" })
@@ -32,20 +31,21 @@ keymap.set("n", "W", "<cmd>:bdelete<cr>", { noremap = false, silent = true, desc
 keymap.set("v", ">", ">gv")
 keymap.set("v", "<", "<gv")
 
-keymap.set("n", "D", "<Cmd>copy.<Cr>",{ noremap = false, silent = true }, { desc = "Duplicate Lines in Normal Mode" })
-keymap.set("x", "D", ":copy.-1<Cr>gv",{ noremap = false, silent = true }, { desc = "Duplicate Lines in Visual Mode" })
+keymap.set("n", "D", "<Cmd>copy.<Cr>", { noremap = false, silent = true }, { desc = "Duplicate Lines in Normal Mode" })
+keymap.set("x", "D", ":copy.-1<Cr>gv", { noremap = false, silent = true }, { desc = "Duplicate Lines in Visual Mode" })
 if vim.o.wrap then
 	keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { expr = true })
 	keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { expr = true })
 end
--- Disable q: 
+-- Disable q:
 keymap.set('n', 'q:', function() end)
 
 -- ╔═══════════════════════╗
 -- ║    Telescope          ║
 -- ╚═══════════════════════╝
 
-keymap.set('n', '<Tab>', builtin.buffers, { desc = 'List Buffers' })
+keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Find Files in current buffer' })
+keymap.set('n', '<leader>g', builtin.grep_string, { desc = 'Enable Grep String' })
 keymap.set("n", "<leader>h", "<cmd>TodoTelescope<cr>", { desc = "Show Todo in Telescope" })
 keymap.set('n', '<leader>o', '<cmd>:Telescope aerial<CR>', { desc = "Toggle Telescope Aerial" })
 keymap.set('n', ':', ':Telescope cmdline<CR>', { noremap = true, desc = "Show Command Line", silent = true })
@@ -62,11 +62,9 @@ keymap.set({ "n", "t" }, "<C-j>", "<CMD>NavigatorDown<CR>")
 -- ║    Other              ║
 -- ╚═══════════════════════╝
 
--- Toggle Neotree
+-- Toggle Neotree/sidebar
 keymap.set('n', '<leader>t', "<CMD>Neotree toggle <CR>")
--- Select highlight from drop box 
+-- Select highlight from drop box
 keymap.set('n', '<leader>/', function()
 	custom_key.pick_todo()
 end, { noremap = true, desc = "Select Highlight From Box" })
-
-
