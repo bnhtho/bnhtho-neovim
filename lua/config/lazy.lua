321b8ccb602c943f916eb5c758729ca64c47f490
Bui Nguyen Hoang Tho <thohnb@gmail.com>
2024-10-03 11:54:53 +0700

    [Dasboard] Add intro

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,7 +1,6 @@
 -- ╔═══════════════════════╗
 -- ║    Bootstrap          ║
 -- ╚═══════════════════════╝
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -36,4 +35,3 @@ require("lazy").setup({
 	},
 	install = { colorscheme = { "gruvbox" } },
 })
-
--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,13 +1,16 @@
 -- Colorscheme
 return {
-    'sainnhe/gruvbox-material',
-    dependencies = {"nvim-tree/nvim-web-devicons"},
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_foreground = "mix"
-            vim.g.gruvbox_material_background = "medium"
-            vim.g.gruvbox_material_better_performance = 1
-            vim.g.gruvbox_material_transparent_background = 1
-    end
+	{
+
+		'sainnhe/gruvbox-material',
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_foreground = "mix"
+			vim.g.gruvbox_material_background = "medium"
+			vim.g.gruvbox_material_better_performance = 1
+			vim.g.gruvbox_material_transparent_background = 1
+		end
+	},
 }
